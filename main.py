7facb74181e7e46e66d859ef77bd1d06a337875f
Cory Dean <corymdean@corymdeanllc.com>
2020-12-18 02:55:37 +0000

    changed the code

--- main.py
+++ main.py
@@ -7,41 +7,53 @@ high = int(input('What is the highest number? '))
 number = random.randint(low,high)
 print(number)
 count = 0
-
+guesses = []
 play = input('Would you like the computer to play for you Yes/No? ')
 if play == 'yes':
-  guess = round(.5*(high-low))
-  count = count + 1
-  print(f'The computers first guess is {guess}')
-if play == 'yes':
-  
-  if guess == number or new_guess== number:
-    print(f'The computer got it, it was {number}.')
-    break
+    guess = round(.5*(1+high-low))
+    count +=1
+    print(f'\nThe computers first guess is {guess}')
 
-  if guess > number:
-    
-      print('Guess was too high, computer will guess again.')
-      guess = new_guess
-        while new_guess > number
-          new_guess = round(.5*(guess-low))
-        if new
-      
-    print(f'next guess is: {guess}')
-    count = count + 1
-    
-  elif guess < number:
-    print('Guess was too low, computer will guess again.')
-    guess = round(guess + .5*(high -guess))
-    print(guess)
-    count = count + 1
-  if count == 10:
-    break
+    if guess == number:
+        print(f'Nice work got it on the first try, it was {guess}')
 
+    if guess > number:
+        guess = guess - round(.5*(guess-low))
+        count +=1
+        print(f'New guess is {guess}')
+        while guess != number:
+            if count == 15:
+                break
+            elif guess > number:
+                guess = guess - round(.5*(guess-low))
+                print(f'New guess is {guess}, this is guess # {count}')
+                count +=1
+            elif guess < number:
+                guess = guess + 1
+                print(f'New guess is {guess}, this is guess # {count}')
+                count +=1
+            
+    if guess < number:
+        guess = guess + round(.5*(high - guess))
+        count +=1
+        print(f'New guess is {guess}, this is guess # {count}')
+        while guess != number:
+            if count == 15:
+                break
+            elif guess < number:
+                guess = guess + round(.5*(high - guess))
+                print(f'New guess is {guess}, this is guess # {count}')
+                count +=1
+            elif guess > number:
+                guess = guess - 1
+                print(f'New guess is {guess}, this is guess # {count}')
+                count +=1
+            
 
 while play == "no":
  
   guess = input(f'What is your guess from {low} and {high} ')
+  guesses.append(guess)
   guess = int(guess)
   count += 1
   if guess > high or guess < low:
@@ -61,3 +73,4 @@ while play == "no":
 
 print(f'\nThanks for playing, the number was: {number}')
 print(f'It took you {count} guesses.')
+print(guesses)
